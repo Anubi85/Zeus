d84beed62f96acc12b533f1cf6188bd2b7b3375b
Andrea Parisotto <andrea.parisotto.85@gmail.com>
2018-05-17 17:41:42 +0200

    [AP] Implement ZeusDialog control

---
+++ Zeus.UI/Controls/ZeusDialog.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Zeus.UI.Controls
+{
+    /// <summary>
+    /// Zeus styled dialog <see cref="Window"/>.
+    /// </summary>
+    public class ZeusDialog : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Overrides metadata for proper style handling.
+        /// </summary>
+        static ZeusDialog()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ZeusDialog), new FrameworkPropertyMetadata(typeof(ZeusDialog)));
+        }
+
+        /// <summary>
+        /// Create a new instance of <see cref="ZeusDialog"/> and initialize its internal structures.
+        /// </summary>
+        public ZeusDialog()
+        {
+            CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, (sender, e) => SystemCommands.CloseWindow(this)));
+            Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri("/Zeus.UI;component/Styles/Zeus.xaml", UriKind.Relative) });
+        }
+
+        #endregion
+
+        #region Dependency properties
+
+        /// <summary>
+        /// <see cref="DependencyProperty"/> that handle window close button visibility.
+        /// </summary>
+        public static readonly DependencyProperty ShowCloseButtonProperty = DependencyProperty.Register("ShowCloseButton", typeof(bool), typeof(ZeusDialog));
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Flag that handle window close button visibility.
+        /// </summary>
+        public bool ShowCloseButton
+        {
+            get { return (bool)GetValue(ShowCloseButtonProperty); }
+            set { SetValue(ShowCloseButtonProperty, value); }
+        }
+
+        #endregion
+    }
+}
