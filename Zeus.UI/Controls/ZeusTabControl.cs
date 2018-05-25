85332b5f8fd92331e4616129b3b544f4e7ccee18
Andrea Parisotto <andrea.parisotto.85@gmail.com>
2018-05-25 13:53:38 +0100

    [AP] Remove unsed code

--- Zeus.UI/Controls/ZeusTabControl.cs
+++ Zeus.UI/Controls/ZeusTabControl.cs
@@ -49,58 +49,6 @@ namespace Zeus.UI.Controls
 
         #endregion
 
-        #region Delegates
-
-        /// <summary>
-        /// Provides the signature for the <see cref="TabItemClosing"/> event handlers.
-        /// </summary>
-        /// <param name="sender">The object that trigger the event.</param>
-        /// <param name="e">The object that contains detail about the event.</param>
-        public delegate void TabItemClosingEventHandler(object sender, TabItemClosingEventArgs e);
-
-        #endregion
-
-        #region Events
-
-        /// <summary>
-        /// An event that is raised when a <see cref="ZeusTabItem"/> is closed.
-        /// </summary>
-        public event TabItemClosingEventHandler TabItemClosing;
-
-        #endregion
-
-        #region Event Args
-
-        /// <summary>
-        /// Event args that is created when a <see cref="ZeusTabItem"/> is closed.
-        /// </summary>
-        public class TabItemClosingEventArgs : CancelEventArgs
-        {
-            #region Constructor
-
-            /// <summary>
-            /// Initialize a new instance of <see cref="TabItemClosingEventArgs"/>.
-            /// </summary>
-            /// <param name="item">The <see cref="ZeusTabItem"/> that trigger the event.</param>
-            internal TabItemClosingEventArgs(ZeusTabItem item)
-            {
-                ClosingTabItem = item;
-            }
-
-            #endregion
-
-            #region Properties
-
-            /// <summary>
-            /// Gets the <see cref="ZeusTabItem"/> that will be closed.
-            /// </summary>
-            public ZeusTabItem ClosingTabItem { get; private set; }
-
-            #endregion
-        }
-
-        #endregion
-
         #region Methods
 
         /// <summary>
@@ -110,47 +58,6 @@ namespace Zeus.UI.Controls
         protected override DependencyObject GetContainerForItemOverride()
         {
             return new ZeusTabItem();
-        }
-        /// <summary>
-        /// Raises the <see cref="TabItemClosing"/> event.
-        /// </summary>
-        /// <param name="closingItem">The <see cref="ZeusTabItem"/> that raise the event.</param>
-        /// <returns>true if the event has been rised, false otherwise.</returns>
-        internal bool RaiseTabItemClosingEvent(ZeusTabItem closingItem)
-        {
-            if (TabItemClosing != null)
-            {
-                foreach (TabItemClosingEventHandler subHandler in TabItemClosing.GetInvocationList().OfType<TabItemClosingEventHandler>())
-                {
-                    TabItemClosingEventArgs args = new TabItemClosingEventArgs(closingItem);
-                    subHandler.Invoke(this, args);
-                    if (args.Cancel)
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-        /// <summary>
-        /// Close the given <see cref="ZeusTabItem"/>.
-        /// </summary>
-        /// <param name="tabItem">The <see cref="ZeusTabItem"/> that has to be closed.</param>
-        internal void CloseThisTabItem(ZeusTabItem tabItem)
-        {
-            if (tabItem == null)
-            {
-                throw new ArgumentNullException(nameof(tabItem));
-            }
-
-            if (CloseTabCommand != null)
-            {
-                object closeTabCommandParameter =  tabItem;
-                if (CloseTabCommand.CanExecute(closeTabCommandParameter))
-                {
-                    CloseTabCommand.Execute(closeTabCommandParameter);
-                }
-            }
         }
 
         #endregion
