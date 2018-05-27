267ef9ffe02c20f4350b9f4448025634de38d640
Andrea Parisotto <andrea.parisotto.85@gmail.com>
2018-05-27 17:58:43 +0100

    [AP] Add ZeusStyleColor support to ZeusMenu and ZeusMenuItem

--- Zeus.UI/Controls/ZeusMenu.cs
+++ Zeus.UI/Controls/ZeusMenu.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using Zeus.UI.Themes.Enums;
 
 namespace Zeus.UI.Controls
 {
@@ -16,6 +17,29 @@ namespace Zeus.UI.Controls
         static ZeusMenu()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ZeusMenu), new FrameworkPropertyMetadata(typeof(ZeusMenu)));
+            ColorProperty = ZeusWindowBase.ColorProperty.AddOwner(typeof(ZeusMenu), new FrameworkPropertyMetadata(ZeusColorStyles.Blue, FrameworkPropertyMetadataOptions.Inherits));
+        }
+
+        #endregion
+
+        #region Dependency properties
+
+        /// <summary>
+        /// <see cref="DependencyProperty"/> that handle <see cref="ZeusMenu"/> color styles.
+        /// </summary>
+        public static readonly DependencyProperty ColorProperty;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets <see cref="ZeusMenu"/> color style.
+        /// </summary>
+        public ZeusColorStyles Color
+        {
+            get { return (ZeusColorStyles)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
         }
 
         #endregion
--- Zeus.UI/Controls/ZeusMenuItem.cs
+++ Zeus.UI/Controls/ZeusMenuItem.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using Zeus.UI.Themes.Enums;
 
 namespace Zeus.UI.Controls
 {
@@ -16,6 +17,29 @@ namespace Zeus.UI.Controls
         static ZeusMenuItem()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ZeusMenuItem), new FrameworkPropertyMetadata(typeof(ZeusMenuItem)));
+            ColorProperty = ZeusWindowBase.ColorProperty.AddOwner(typeof(ZeusMenuItem), new FrameworkPropertyMetadata(ZeusColorStyles.Blue, FrameworkPropertyMetadataOptions.Inherits));
+        }
+
+        #endregion
+
+        #region Dependency properties
+
+        /// <summary>
+        /// <see cref="DependencyProperty"/> that handle <see cref="ZeusMenuItem"/> color styles.
+        /// </summary>
+        public static readonly DependencyProperty ColorProperty;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets <see cref="ZeusMenuItem"/> color style.
+        /// </summary>
+        public ZeusColorStyles Color
+        {
+            get { return (ZeusColorStyles)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
         }
 
         #endregion
