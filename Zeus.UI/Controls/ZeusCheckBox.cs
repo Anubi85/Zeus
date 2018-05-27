77a25941497a9def1764631a8656794f1f5f247e
Andrea Parisotto <andrea.parisotto.85@gmail.com>
2018-05-27 16:08:53 +0100

    [AP] Implement the ZeusCheckBox

---
+++ Zeus.UI/Controls/ZeusCheckBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Zeus.UI.Controls
+{
+    /// <summary>
+    /// Zeus styled <see cref="CheckBox"/>.
+    /// </summary>
+    public class ZeusCheckBox : CheckBox
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Overrides metadata for proper style handling.
+        /// </summary>
+        static ZeusCheckBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ZeusCheckBox), new FrameworkPropertyMetadata(typeof(ZeusCheckBox)));
+        }
+
+        #endregion
+    }
+}
