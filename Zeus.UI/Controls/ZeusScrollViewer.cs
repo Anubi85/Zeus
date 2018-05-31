98a9ca14479feb171177a55b3aaea5fbc9809e7c
Andrea Parisotto <andrea.parisotto.85@gmail.com>
2018-05-31 19:39:44 +0100

    [AP] Implemented ZeusScrollViewer control

---
+++ Zeus.UI/Controls/ZeusScrollViewer.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Zeus.UI.Themes.Enums;
+
+namespace Zeus.UI.Controls
+{
+    /// <summary>
+    /// Zeus styled <see cref="ScrollViewer"/>.
+    /// </summary>
+    public class ZeusScrollViewer : ScrollViewer
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Overrides metadata for proper style handling.
+        /// </summary>
+        static ZeusScrollViewer()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ZeusScrollViewer), new FrameworkPropertyMetadata(typeof(ZeusScrollViewer)));
+            ColorProperty = ZeusWindowBase.ColorProperty.AddOwner(typeof(ZeusScrollViewer), new FrameworkPropertyMetadata(ZeusColorStyles.Blue, FrameworkPropertyMetadataOptions.Inherits));
+        }
+
+        #endregion
+
+        #region Dependency properties
+
+        /// <summary>
+        /// <see cref="DependencyProperty"/> that handle <see cref="ZeusScrollViewer"/> color styles.
+        /// </summary>
+        public static readonly DependencyProperty ColorProperty;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets <see cref="ZeusScrollViewer"/> color style.
+        /// </summary>
+        public ZeusColorStyles Color
+        {
+            get { return (ZeusColorStyles)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+
+        #endregion
+    }
+}
