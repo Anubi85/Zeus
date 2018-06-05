ab5cb1d01ae6a5e437a0dab1504263a696ef1848
Andrea Parisotto <andrea.parisotto.85@gmail.com>
2018-06-05 13:52:25 +0100

    [AP] Implement Calendar icon

---
+++ Zeus.UI/Controls/ZeusDateTimePicker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Zeus.UI.Themes.Enums;
+
+namespace Zeus.UI.Controls
+{
+    /// <summary>
+    /// Zeus styled <see cref="DatePicker"/>.
+    /// </summary>
+    public class ZeusDateTimePicker : DatePicker
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Overrides metadata for proper style handling.
+        /// </summary>
+        static ZeusDateTimePicker()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ZeusDateTimePicker), new FrameworkPropertyMetadata(typeof(ZeusDateTimePicker)));
+            ColorProperty = ZeusWindowBase.ColorProperty.AddOwner(typeof(ZeusDateTimePicker), new FrameworkPropertyMetadata(ZeusColorStyles.Blue, FrameworkPropertyMetadataOptions.Inherits));
+        }
+
+        #endregion
+
+        #region Dependency properties
+
+        /// <summary>
+        /// <see cref="DependencyProperty"/> that handle <see cref="ZeusDateTimePicker"/> color styles.
+        /// </summary>
+        public static readonly DependencyProperty ColorProperty;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets <see cref="ZeusDateTimePicker"/> color style.
+        /// </summary>
+        public ZeusColorStyles Color
+        {
+            get { return (ZeusColorStyles)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+
+        #endregion
+    }
+}
